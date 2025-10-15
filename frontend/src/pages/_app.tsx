54a9d4572a07a60398206dd9298bad3b07c55e7e
Nolimiter <rustbestes@gmail.com>
2025-10-15 22:33:39 +0300

    Remove NextAuth SessionProvider to fix auth errors

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,19 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
 import { AuthProvider } from '../contexts/AuthContext';
 import { GameProvider } from '../contexts/GameContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { session, ...restPageProps } = pageProps;
   return (
-    <SessionProvider session={session}>
-      <AuthProvider>
-        <GameProvider>
-          <Component {...restPageProps} />
-        </GameProvider>
-      </AuthProvider>
-    </SessionProvider>
+    <AuthProvider>
+      <GameProvider>
+        <Component {...pageProps} />
+      </GameProvider>
+    </AuthProvider>
   );
 }
 
